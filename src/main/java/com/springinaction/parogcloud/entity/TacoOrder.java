0d0d15f91ca3afbc8e849e79bfebff5fc83cc31f
Alex Parog <sbibik2000@mail.ru>
2024-10-25 17:10:52 +0300

    chapter 2, feat: расширена объектная модель

--- src/main/java/com/springinaction/parogcloud/entity/TacoOrder.java
+++ src/main/java/com/springinaction/parogcloud/entity/TacoOrder.java
@@ -13,6 +13,7 @@ public class TacoOrder {
     private String deliveryName;
     private String deliveryStreet;
     private String deliveryCity;
+    private String deliveryState;
     private String deliveryZip;
     private String ccNumber;
     private String ccExpiration;
