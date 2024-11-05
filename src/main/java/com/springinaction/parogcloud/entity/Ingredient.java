217c905a8f44067346767de9ed61a9235fe38e89
Alex Parog <sbibik2000@mail.ru>
2024-11-05 15:46:08 +0300

    chapter 3, feat: быстрое введение в Spring Data JPA

--- src/main/java/com/springinaction/parogcloud/entity/Ingredient.java
+++ src/main/java/com/springinaction/parogcloud/entity/Ingredient.java
@@ -1,21 +1,23 @@
 package com.springinaction.parogcloud.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.NoArgsConstructor;
 
 /**
  * Класс, представляющий ингредиенты тако.
  */
 @Data
-@Table
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Ingredient {
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/com/springinaction/parogcloud/entity/Taco.java
+++ src/main/java/com/springinaction/parogcloud/entity/Taco.java
@@ -1,9 +1,12 @@
 package com.springinaction.parogcloud.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
@@ -13,7 +16,7 @@ import java.util.List;
  * Класс, представляющий рецепт.
  */
 @Data
-@Table
+@Entity
 public class Taco {
     @Id
     private Long id;
@@ -22,6 +25,7 @@ public class Taco {
     private String name;
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
+    @ManyToMany
     private List<Ingredient> ingredients;
     private Date createdAt = new Date();
 }
